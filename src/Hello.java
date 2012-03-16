0ac894fba326b85d7abee224627c8013ab6e0181
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2012-03-16 12:31:53 +0100

    Documentation improvements.
    Signed-off-by: Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 
 public class Hello {
+	/** Implement the application
+	 * 
+	 * @param args arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World! (ñ)");
 	}
